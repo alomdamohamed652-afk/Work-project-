2b347f5d83885140f51e63b9a3148eca0ced4744
alomdamohamed652-afk <alomdamohamed652@gmail.com>
2026-09-07 01:39:53 +0300

    cleanup: remove obsolete AsyncStorage import from restaurants

--- app/customer/restaurants.tsx
+++ app/customer/restaurants.tsx
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import{router,useLocalSearchParams}from'expo-router';
 import{useEffect,useState}from'react';
 import{Image,Pressable,RefreshControl,ScrollView,StyleSheet,Text,TextInput,View}from'react-native';
